aec24eb0b8d6707bbb47be22e1192996ddb7755b
Tom Jooris <tjooris@z3r2p3.42lyon.fr>
2025-03-18 14:28:21 +0100

    ajout de struct

--- include/ft_philosophers.h
+++ include/ft_philosophers.h
@@ -6,7 +6,7 @@
 /*   By: tjooris <tjooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 14:06:45 by tjooris           #+#    #+#             */
-/*   Updated: 2025/03/11 14:08:37 by tjooris          ###   ########.fr       */
+/*   Updated: 2025/03/18 09:54:46 by tjooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,23 @@
 #include "libft.h"
 #include <pthread.h>
 #include <stdlib.h>
+
+enum	t_state
+{
+	SLEEP,
+	EAT,
+	THINK,
+};
+
+typedef	struct s_phylosopher
+{
+	int	number;
+	int state;
+}	t_phylosopher;
+
+typedef struct s_table
+{
+	
+}	t_table;
 
 #endif
--- src/ft_philosophers.c
+++ src/ft_philosophers.c
@@ -6,13 +6,24 @@
 /*   By: tjooris <tjooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 14:07:41 by tjooris           #+#    #+#             */
-/*   Updated: 2025/03/12 14:57:40 by tjooris          ###   ########.fr       */
+/*   Updated: 2025/03/13 15:17:17 by tjooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philosophers.h"
 
+void init_phylosophers(t_phylosopher **phylo, int argc)
+{
+	phylo = malloc (sizeof(t_phylosopher) * argc);
+	if (!phylo)
+		ft_error("erreur d'allocation");
+}
+
 int main(int argc, char **argv)
 {
+	int i;
+	t_phylosopher	*phylosophers;
+
+	init_phylosophers(phylosophers, argc);
 	
 }
