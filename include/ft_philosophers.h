573b201626bf435c43bd2e5ae36843d9bed2f75d
Tom Jooris <tjooris@z2r4p1.42lyon.fr>
2025-03-19 13:57:25 +0100

    all

--- include/ft_philosophers.h
+++ include/ft_philosophers.h
@@ -6,7 +6,7 @@
 /*   By: tjooris <tjooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 14:06:45 by tjooris           #+#    #+#             */
-/*   Updated: 2025/03/18 09:54:46 by tjooris          ###   ########.fr       */
+/*   Updated: 2025/03/19 11:03:28 by tjooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include "libft.h"
 #include <pthread.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 enum	t_state
 {
@@ -24,15 +25,23 @@ enum	t_state
 	THINK,
 };
 
-typedef	struct s_phylosopher
-{
-	int	number;
-	int state;
-}	t_phylosopher;
+typedef struct s_table {
+    int nb_philo;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int must_eat;
+    pthread_mutex_t *forks;
+    pthread_mutex_t print_lock;
+    long start_time;
+} t_table;
 
-typedef struct s_table
-{
-	
-}	t_table;
+typedef struct s_philosopher {
+    int id;
+    int meals_eaten;
+    long last_meal_time;
+    pthread_t thread;
+    t_table *table;
+} t_philosopher;
 
 #endif
--- src/ft_philosophers.c
+++ src/ft_philosophers.c
@@ -6,24 +6,17 @@
 /*   By: tjooris <tjooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 14:07:41 by tjooris           #+#    #+#             */
-/*   Updated: 2025/03/13 15:17:17 by tjooris          ###   ########.fr       */
+/*   Updated: 2025/03/19 13:47:36 by tjooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philosophers.h"
 
-void init_phylosophers(t_phylosopher **phylo, int argc)
-{
-	phylo = malloc (sizeof(t_phylosopher) * argc);
-	if (!phylo)
-		ft_error("erreur d'allocation");
-}
-
 int main(int argc, char **argv)
 {
 	int i;
-	t_phylosopher	*phylosophers;
 
-	init_phylosophers(phylosophers, argc);
+	if (argc < 5 || argc > 6)
+		ft_error("erreur, nombre d'arguments invalide");
 	
 }
