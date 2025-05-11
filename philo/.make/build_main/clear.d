8729ab3eec38c413659f7c20ba762edd4a345f30
Rabbras <tom.jooris@hotmail.fr>
2025-05-11 22:28:42 +0200

    adding some correction

--- philo/.make/build_main/clear.d
+++
@@ -1,5 +0,0 @@
-.make/build_main/clear.o: src/clear.c include/ft_philosophers.h \
- lib/libft/include/libft.h lib/libft/include/ft_printf.h
-include/ft_philosophers.h:
-lib/libft/include/libft.h:
-lib/libft/include/ft_printf.h:
