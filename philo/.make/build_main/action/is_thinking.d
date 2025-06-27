ebc280c84035858e78a8d5c9fbaba6351015299f
Tom Jooris <tjooris@z4r9p2.42lyon.fr>
2025-06-27 14:40:03 +0200

    fixing one another mistake

--- philo/.make/build_main/action/is_thinking.d
+++
@@ -1,4 +0,0 @@
-.make/build_main/action/is_thinking.o: src/action/is_thinking.c \
-  include/ft_philosophers.h
-
-include/ft_philosophers.h:
--- philo/.make/build_main/init.d
+++
@@ -1,3 +0,0 @@
-.make/build_main/init.o: src/init.c include/ft_philosophers.h
-
-include/ft_philosophers.h:
